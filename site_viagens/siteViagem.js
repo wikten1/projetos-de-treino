e598729d04eb36d9aecfc2f6db34ccc63cb29f0a
Wikten Alves <wiktenalves@gmail.com>
2023-12-27 01:45:28 -0300

    modificação no iframe
    Fiz o iframe trocar de acordo com os botões de pacote de viagem escolhidos pelo usuário.

---
+++ site_viagens/siteViagem.js
@@ -0,0 +1,23 @@
+function trocaGuarapari(){
+    video = document.getElementById('iframe-video')
+    video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/QSXEaHYbkG4?si=y0nO7OXD9PqS0vyY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+}
+
+function trocaParis(){
+    var video = document.getElementById('iframe-video');
+    video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/mmA242kQUkw?si=NMG4CCe-H4OyhETy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
+
+function trocaBuzios(){
+    var video = document.getElementById('iframe-video');
+    video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/OWaNzFU3yik?si=Tno0E6IMB5LxVREb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
+
+function trocaLondres(){
+    var video = document.getElementById('iframe-video');
+    video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/OR6ZaL732p8?si=ygbg-b5JREf4MKHd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
+
+
+
+
